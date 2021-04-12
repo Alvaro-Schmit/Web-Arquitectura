c9f239e61b16d2ff93662f73d3845bb4c99c1fa5
Alvaro Schmit <alvaroschmit@gmail.com>
2021-04-12 19:15:56 -0300

    cambios en cerrar fotos lightbox

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$('.galeria-imagen').click(function(parametro) {
-    var img = parametro.target.src;
-    var modal = '<div class="modal1"><img class="galeria_imagen" src="' + img + '" alt=""><div class="modal1__boton">x</div></div>';
-
-    $('body').append(modal);
-})
-console.log('funciona')
-
-var prueba = ('ahora funciona')
