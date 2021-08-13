069650c017a8ca1ca3b64ac95b4692581387103d
Alvaro Schmit <alvaroschmit@gmail.com>
2021-08-13 19:21:19 -0300

    Ajuste  de links

---
+++ js/dataGalerys/olivaLocalData.js
@@ -0,0 +1,60 @@
+let olivaLocalData = [
+    {
+       'id':'aaaa',
+       'href':'#img1',
+       'picture':'../image/casas/Oliva/11.jpg'
+    },
+    {
+        'id':'aaab',
+        'href':'#img2',
+        'picture':'../image/casas/Oliva/3.jpg'
+     },
+     {
+        'id':'aaac',
+        'href':'#img3',
+        'picture':'../image/casas/Oliva/4.jpg'
+     },
+     {
+        'id':'aaad',
+        'href':'#img4',
+        'picture':'../image/casas/Oliva/4245.jpg'
+     },
+     {
+        'id':'aaae',
+        'href':'#img5',
+        'picture':'../image/casas/Oliva/4271.jpg'
+     },
+     {
+        'id':'aaaf',
+        'href':'#img6',
+        'picture':'../image/casas/Oliva/4279.jpg'
+     },
+     {
+        'id':'aaag',
+        'href':'#img7',
+        'picture':'../image/casas/Oliva/4282.jpg'
+     },
+     {
+        'id':'aaah',
+        'href':'#img8',
+        'picture':'../image/casas/Oliva/4285.jpg'
+     },
+     {
+        'id':'aaai',
+        'href':'#img9',
+        'picture':'../image/casas/Oliva/5.jpg'
+     }
+
+]
+
+
+
+   
+   
+   
+   
+   let idGaleryOlivaLocal =document.getElementById("idGaleryOlivaLocal")
+   loadGalery(idGaleryOlivaLocal,olivaLocalData)
+
+
+
