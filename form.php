717b8be44ecaaa4dd17cbb65d539791ea320ca02
Alvaro Schmit <alvaroschmit@gmail.com>
2021-08-17 20:02:03 -0300

    prueba con archivo php

---
+++ form.php
@@ -0,0 +1,24 @@
+<?php
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $fname . " \r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Teléfono de contacto: " . $lname . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'alvaroschmit@hotmail.com';
+$asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
+
+email($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
